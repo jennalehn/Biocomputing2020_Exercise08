e4eea3c23051d11f2972d182d5734f74f035d1a6
jennalehn <jenna.lehn@gmail.com>
2020-10-22 13:04:01 -0400

    it's done! yay!

--- Ex8.R
+++ Ex8.R
@@ -28,63 +28,31 @@ plot(UWtime,UWcm, type="l",
 lines(MSUtime,MSUcm, type="l")
 legend("topleft", c("UW","MSU"), fill=c("blue","black"))
 
-#done for now help me
-output=numeric(length=length(v1))
-v1=(c(3,10,4,12,55))
-for (i in 1:length(v1)){
-  output[i]=sum(v1[1:i])
-}
-UWtime=0
-UWcum=0
-UWcum=numeric(length=length(UWscores))
-MSUcum=0
-timeUW=data.frame(x=numeric(i), stringsAsFactors = F)
-for(i in 1:nrow(bball)){
-  if (bball$team[i]=="UW"){
-  UWcum[i]=sum(bball$score[1:i], na.rm=TRUE)
-  UWtime=bball$time[1:i]
-  }else{
-  MSUcum=cumsum(bball$score[i])
-  }}
-UWcum
-UWfinscore=UWcum[!is.na(UWcum)]
-UWfinscore
-UWtime
-MSUcum
-UWscores
-sum(UWscores)
-sum(bball$scores)
-timeUW
-?data.frame()
-class(UWcum)
-
 # 2. write guess my number game
-# create vector from 1 to 100
-allnumbers=sample(1:100, 1)
-allnumbers
-
+# create function to guess numbers
 
 game<-function(guess) {
   cat("I'm thinking of a number 1-100...")
   answer=sample(1:100, 1) # generates a random # from 1-100
-  print(answer)
-  guess=readline(prompt="Guess: ") # gives the prompt for game
-  count=0
+  guess=as.numeric(readline(prompt="Guess: ")) # gives the prompt for game
+  count=1 # variable to count number of attempts
+    
   while (guess!=answer){ # gives the game multiple guesses
-    count=count+1
-    if (count==10)
-      break
-    if (guess > answer){
+    if (guess > answer){ # if guess is higher than answer, say "lower" and guess again
     cat("Lower")
-    guess=readline(prompt="Guess: ")
-  }else if (guess < answer){
+    guess=as.numeric(readline(prompt="Guess: "))
+    }else { # if guess is lower than answer, say "higher" and guess again
     cat("Higher")
-    guess=readline(prompt="Guess: ")
-  }else cat("Correct!")
-  } 
+    guess=as.numeric(readline(prompt="Guess: "))
+    }
+    
+    if (guess==answer){ # if guess is correct, say "correct" and game ends 
+      cat("Correct!")
+    }
+    
+  count=count+1# counts number of attempts every time a guess is made
+  if (count==10) # when 10 guesses are made, the game ends
+    break
+    } 
 }
-game()
-
-
-
-
+game() # run game
